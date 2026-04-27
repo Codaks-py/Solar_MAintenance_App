f45d5837851ab531de14f13450671680506eecf4
Codaks <Peteroluwasegun2002@gmail.com>
2026-04-27 08:49:22 +0100

    added a few comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ class_model, reg_model = load_models()
 st.sidebar.header("Sensor Inputs")
 
 
-
+#The user input
 DC_POWER = st.sidebar.number_input("DC Power (W)")
 AC_POWER = st.sidebar.number_input("AC_Power (W)")
 HOUR = st.sidebar.number_input("Hour of the Day", max_value=23)
@@ -35,10 +35,14 @@ IRRADIATION = st.sidebar.number_input("Irradiation (W/m)")
 
 
 
-
+#setting threshold for the rule-based classification
 normal_threshold = 0
 fault_threshold =  0.4
 
+#based on the model, the predict_proba will be used to get the probability that an abnormality will occur
+#Once the probability that an abnormality will occur, the final status function checkes if the less than the 
+#the threshold aand also if the performance drop is zero or greater to give a normal signal and 
+# for fault if the probability is higher than the fault threshold and for the rest for warning
 
 def final_status(drop, prob):
     
@@ -49,9 +53,9 @@ def final_status(drop, prob):
         else:
             return 'Warning'
 
-
+#this defines the the passing of the vallues into the the model to make prediction once the button is clicked
 if st.button("Make Prediction"):
-
+    #passes the user input into the model
     data = pd.DataFrame([{
         'DC_POWER' : DC_POWER,
         'AC_POWER' : AC_POWER,
@@ -71,21 +75,27 @@ if st.button("Make Prediction"):
     PERFORMANCE_DROP = abs((PERFORMANCE_RATIO - reg_pred) / (reg_pred + 0.0000001))
     status = final_status(PERFORMANCE_DROP, cls_pred)
     
+    #The formalu behinh the performace drop is calculated as shown above, and the reason is been that the performance ratio can be claculated manually without the need of a predictor
+    #  and it was calculated and a predicted perfomance was gotten too. 
+    # A comparison needs to be done to check how well is it to perform then. If the predicted value is greater than actual then there's an issue
+    # this means that the system overperformed it actual performance meaning a surge in the intake of current, voltage or maybe the controller in the inverter malfunctioned
+    # all of this cases are said to be true if the predicted value exceed the actual values.
+    # The Performance drop has to be positive to have a good system.
 
 
-
-
+    #this outputs the calculated values
     st.subheader("Output")
 
     col1, col2, col4, col3  = st.columns(4)
-
+    #they are arranged in the order of presentation
     col1.metric('Performance Ratio Drop', round(PERFORMANCE_DROP, 3))
     col2.metric('Abnormality Probability', round(cls_pred, 3))
     col3.metric("Predicted PR", round(reg_pred, 2))
     col4.metric('Actual PR', round(PERFORMANCE_RATIO, 2))
 
 
-
+    #using streamlit success function to give the signals, 
+    #just like red means serious alert, green means all is fine and yellow gives a warning signal
     st.subheader("🚦 System Status")
 
     if status == "Normal":
@@ -96,6 +106,6 @@ if st.button("Make Prediction"):
         st.error("🔴 Fault Detected")
 
 
-
+#EnD
 
   
